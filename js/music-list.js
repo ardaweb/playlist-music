d48f1aad9578cf71c82e56197d70ec756e47ab02
ardaweb <websitearda@gmail.com>
2022-08-29 06:09:16 +0700

    playlist-music

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "DJ GEMES AKU X MAMA BALE BALE V2 MENGKANE VIRAL FYP TIKTOK",
+    artist: "VINKY YETE",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "DJ ALREADY GONE VIRAL TIK TOK TERBARU 2022",
+    artist: "ELLKHA BLOODS ",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "DJ ELA ELA SLOW REMIX VIRAL TIKTOK TERBARU",
+    artist: "Dj  Ardyartha",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "DJ LET HER GO JEDAG JEDUG FULL BEAT VIRAL TIKTOK TERBARU 2022",
+    artist: "DJ KOMANG RIMEX",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "DJ AISYAH BUKAN BONEKA BASS BEAT",
+    artist: "FELIK FVNKY",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Dj Ular V9 X Pyramid Sound JJ",
+    artist: "Raka Remixer",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
